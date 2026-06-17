1d420a4de60af42ba3d21c1ab21f9493a3603756
LimdaeIl <optional94@naver.com>
2026-06-17 13:18:57 +0900

    ✨#35: AuditorAware 정보를 SecurityContextHolder에서 획득

--- backend/src/main/java/com/app/backend/common/audit/AuditorAwareImpl.java
+++ backend/src/main/java/com/app/backend/common/audit/AuditorAwareImpl.java
@@ -1,16 +1,32 @@
 package com.app.backend.common.audit;
 
+import com.app.backend.auth.infrastructure.security.AuthMember;
 import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component("auditorAware")
-public class AuditorAwareImpl implements AuditorAware<String> {
+public class AuditorAwareImpl implements AuditorAware<Long> {
 
-    private static final String SYSTEM = "SYSTEM";
+    private static final Long SYSTEM = 0L;
 
     @Override
-    public Optional<String> getCurrentAuditor() {
-        return Optional.empty();
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof AuthMember authMember) {
+            return Optional.of(authMember.memberId());
+        }
+
+        return Optional.of(SYSTEM);
+
     }
 }
