f602939770a47f9b75a75955d22010a4f16fa2b4
LimdaeIl <optional94@naver.com>
2026-06-17 13:06:04 +0900

    ✨#33: EntryPoint, DeniedHandler 구현
    401 인증 실패는 EntryPoint, 403 권한 부족은 DeniedHandler 에서 처리할 수 있도록 구현.
    
    만약, 구현하지 않으면 적절한 예외 없이 대부분 500 에러 발생으로 원인 파악 어려움.

--- backend/src/main/java/com/app/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/app/backend/common/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.app.backend.common.config;
 
+import com.app.backend.auth.infrastructure.security.JwtAccessDeniedHandler;
+import com.app.backend.auth.infrastructure.security.JwtAuthenticationEntryPoint;
+import com.app.backend.auth.infrastructure.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +12,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,13 +32,19 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
-
-        http
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
